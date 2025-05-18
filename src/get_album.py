b585b1495753a36e646f4c64cb824f5a5124448a
Huang207 <huang207a@outlook.com>
2025-05-19 04:04:44 +0800

    use ffi fetch

--- src/get_album.py
+++ src/get_album.py
@@ -1,4 +1,12 @@
-import aiohttp
+# import aiohttp
+from workers import fetch, handler
+import json
+
+
+@handler
+async def get_handler(url):
+    return await fetch(url)
+
 
 urls = {
     "albums": "https://monster-siren.hypergryph.com/api/albums",
@@ -8,6 +16,7 @@ urls = {
     "song": "https://monster-siren.hypergryph.com/api/song/{}",
 }
 
+
 async def get_album(album_id):
     """
     Get the album details from the API.
@@ -15,25 +24,35 @@ async def get_album(album_id):
     album_url = urls["albumDetail"].format(album_id)
 
     # Get the datas of the album
-    async with aiohttp.ClientSession() as session:
-        async with session.get(urls["albumData"].format(album_id)) as response:
-            if response.status != 200:
-                raise Exception(f"Failed to fetch album details: {response.status}")
-            json_data = await response.json()
-            album_datas = json_data["data"]
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.get(urls["albumData"].format(album_id)) as response:
+    #         if response.status != 200:
+    #             raise Exception(f"Failed to fetch album details: {response.status}")
+    #         json_data = await response.json()
+    #         album_datas = json_data["data"]
+
+    response = await get_handler(urls["albumData"].format(album_id))
+    if response.status != 200:
+        raise Exception(f"Failed to fetch album details: {response.status}")
+    json_data = await response.json()
+    album_datas = json_data["data"]
 
     if "code" not in json_data or json_data["code"] != 0:
         raise Exception(f"Failed to fetch album datas: {album_id}")
 
     # Get the details of the album
-    async with aiohttp.ClientSession() as session:
-        async with session.get(album_url) as response:
-            if response.status != 200:
-                raise Exception(f"Failed to fetch album details: {response.status}")
-            json_data = await response.json()
-            album_details = json_data["data"]
-    # response = requests.get(album_url)
-    # album_details = response.json()["data"]
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.get(album_url) as response:
+    #         if response.status != 200:
+    #             raise Exception(f"Failed to fetch album details: {response.status}")
+    #         json_data = await response.json()
+    #         album_details = json_data["data"]
+
+    response = await get_handler(album_url)
+    if response.status != 200:
+        raise Exception(f"Failed to fetch album details: {response.status}")
+    json_data = await response.json()
+    album_details = json_data["data"]
 
     if "code" not in json_data or json_data["code"] != 0:
         raise Exception(f"Failed to fetch album details: {album_id}")
@@ -50,7 +69,7 @@ async def get_album(album_id):
         "intro": album_details["intro"],
         "belong": album_details["belong"],
         "cover": album_cover,
-        "songs": []
+        "songs": [],
     }
 
     # Iterate through each song in the album and get its details
@@ -61,24 +80,30 @@ async def get_album(album_id):
         song_url = urls["song"].format(song_id)
 
         # Get the details of the song
-        async with aiohttp.ClientSession() as session:
-            async with session.get(song_url) as response:
-                if response.status != 200:
-                    raise Exception(f"Failed to fetch song details: {response.status}")
-                json_data = await response.json()
-                song_details = json_data["data"]
-        # response = requests.get(song_url)
-        # song_details = response.json()["data"]
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get(song_url) as response:
+        #         if response.status != 200:
+        #             raise Exception(f"Failed to fetch song details: {response.status}")
+        #         json_data = await response.json()
+        #         song_details = json_data["data"]
+
+        response = await get_handler(song_url)
+        if response.status != 200:
+            raise Exception(f"Failed to fetch song details: {response.status}")
+        json_data = await response.json()
+        song_details = json_data["data"]
 
         if "code" not in json_data or json_data["code"] != 0:
             continue
         # Store the song data in the dictionary
-        album_data["songs"].append({
-            "id": song_id,
-            "name": song_name,
-            "artistes": song_artistes,
-            "url": song_details["sourceUrl"],
-            "lyric": song_details["lyricUrl"]
-        })
+        album_data["songs"].append(
+            {
+                "id": song_id,
+                "name": song_name,
+                "artistes": song_artistes,
+                "url": song_details["sourceUrl"],
+                "lyric": song_details["lyricUrl"],
+            }
+        )
 
     return album_data
